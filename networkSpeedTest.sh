c60044f686f4a610dd18ce223172fa5f57df0339
Marshall Asch <maasch@rogers.com>
2018-03-31 11:16:46 -0400

    updated variable in networkSpeedTest.sh to remove my server name

--- networkSpeedTest.sh
+++ networkSpeedTest.sh
@@ -2,7 +2,8 @@
 
 
 host="hostName"
+server="https://myserver.com/networkLog.php"
 
 b=$(s=$(./speedtest-cli --simple |  sed  -e 's/: /\":\"/g' -e 's/$/\",\"/g') && echo $s | sed -e 's/^/{\"/g' -e 's/\",\" /\",\"/g' -e 's/\",\"$/\",\"host\":\"'"$host"'\"}/g')
 echo " test: $b"
-curl -X POST https://hermes.marshallasch.ca/networkLog.php -d "$b"
+curl -X POST "$server" -d "$b"
